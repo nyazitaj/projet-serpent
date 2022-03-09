0e1f3c14f07f134e1165ea936c316232e3be83ae
David-ODB57 <davidbroccoli@gmail.com>
2022-03-09 08:36:39 +0100

    solved problem with index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,25 @@
 const board = document.querySelector('#board');
-        let tblBody = document.createElement("tbody");
-        let cases = createNum();
+let tblBody = document.createElement("tbody");
 
-        function createNum(num) {
+    for (var j = 0; j < 5 ; j++) {
 
-        }
+        var row = document.createElement("tr");
 
-        for (var j = 0; j < 5 ; j++) {
-
-            var row = document.createElement("tr");
-
-            for (var i = 1; i <= 10; i++) {
+        for (var i = 1; i <= 10; i++) {
                 
-                if(j%2 == 0) {
-                    var cell = document.createElement("td");
-                    var cellText = document.createTextNode(i+j*10);
-                }
-                else {
-                    var cell = document.createElement("td");
-                    var cellText = document.createTextNode(11-i+j*10);
-                }
+            var cell = document.createElement("td");
+                
+            if(j%2 == 0) {
+                var cellText = document.createTextNode(i+j*10);
+            }
+            else {
+                var cellText = document.createTextNode(11-i+j*10);
+            }
 
-                cell.appendChild(cellText);
-                row.appendChild(cell);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
             }
 
         tblBody.appendChild(row);
         board.appendChild(tblBody);
-            }
-
-        createBoard(5, 50);
+    }
