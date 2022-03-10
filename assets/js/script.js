1a8f6bf4636b72492735a5df679caf345e2e2978
severine <you@example.com>
2022-03-10 13:23:42 +0100

    ✨add update css js

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,61 +25,61 @@ let tblBody = document.createElement("tbody");
     }
 
 
-    // Function use to dynamically create the board with numbers from 1 to 100 or 50 according to the given parameters.
-    function createBoard2(table_id, total_num, max_on_line, direction) {
+// Function use to dynamically create the board with numbers from 1 to 100 or 50 according to the given parameters.
+function createBoard2(table_id, total_num, max_on_line, direction) {
 
-        var max_on_line_new = max_on_line;
+    var max_on_line_new = max_on_line;
 
-        var elem_table = document.createElement('table');
-        elem_table.setAttribute('class', 'board-container');
+    var elem_table = document.createElement('table');
+    elem_table.setAttribute('class', 'board-container');
 
-        var elem_tr = document.createElement('tr');
+    var elem_tr = document.createElement('tr');
 
-        for (x = 1; x <= total_num; x++) {
+    for (x = 1; x <= total_num; x++) {
 
-            // Creating <td> element.
-            var elem_td = document.createElement('td');
-            elem_td.innerHTML = x;
+        // Creating <td> element.
+        var elem_td = document.createElement('td');
+        elem_td.innerHTML = x;
 
-            // Appending the <td> to <tr> according to the selected directions
-            if(direction == 'right') {
-                elem_tr.appendChild(elem_td);
-            }
-            else {
-                elem_tr.insertBefore(elem_td, elem_tr.children[0]);
-            }
+        // Appending the <td> to <tr> according to the selected directions
+        if (direction == 'right') {
+            elem_tr.appendChild(elem_td);
+        }
+        else {
+            elem_tr.insertBefore(elem_td, elem_tr.children[0]);
+        }
 
-            // If the numbers per line reache to the end, then we recreate the <tr> and update the "direction" variable.
-            if(x == max_on_line_new && max_on_line_new < total_num) {
+        // If the numbers per line reache to the end, then we recreate the <tr> and update the "direction" variable.
+        if (x == max_on_line_new && max_on_line_new < total_num) {
 
-                elem_tr = document.createElement('tr');
-                max_on_line_new += max_on_line;
-
-                elem_table.insertBefore(elem_tr, elem_table.children[0]);
-
-                // Updating the "direction" variable.
-                if(direction == 'right') {
-                    direction = 'left';
-                }
-                else {
-                    direction = 'right';
-                }
-
-            }
+            elem_tr = document.createElement('tr');
+            max_on_line_new += max_on_line;
 
             elem_table.insertBefore(elem_tr, elem_table.children[0]);
 
+            // Updating the "direction" variable.
+            if (direction == 'right') {
+                direction = 'left';
+            }
+            else {
+                direction = 'right';
+            }
+
         }
 
-        document.getElementById(table_id).appendChild(elem_table);
+        elem_table.insertBefore(elem_tr, elem_table.children[0]);
 
     }
 
-    createBoard2('snake-table', 50, 10, 'right');
+    document.getElementById(table_id).appendChild(elem_table);
 
-    // Function used to randomly generate number from 1-6
-    function userAction(e) {
-        e.preventDefault();
-        document.getElementById('screen').innerHTML = "";
-        document.getElementById('screen').innerHTML = Math.floor((Math.random() * 6) + 1);
-    }
+}
+
+createBoard2('snake-table', 50, 10, 'right');
+
+// Function used to randomly generate number from 1-6
+function userAction(e) {
+    e.preventDefault();
+    document.getElementById('screen').innerHTML = "";
+    document.getElementById('screen').innerHTML = Math.floor((Math.random() * 6) + 1);
+}
